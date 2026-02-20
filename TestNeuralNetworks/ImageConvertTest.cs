e4a6ef41fd0da5429d253fd1c2400ed8df9c45a4
CsCppKotlin <CsCppKotlin@gmail.com>
2026-02-20 11:13:00 +0300

    Преобразование изображения в черно белый формат

---
+++ TestNeuralNetworks/ImageConvertTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MNeuralNetworks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TestNeuralNetworks
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void ConvertTest()
+        {
+            var converter = new PictureConverter();
+
+            var inputs = converter.Convert(@"C:\\Users\\RZA\\source\\repos\\OopNeuralNetworks\\TestNeuralNetworks\\Images\\Parasitized.png");
+            converter.Save("c:\\image.png", inputs);
+        }
+    }
+}
